8d4a25661813bed0b74077c882af614e6fd6ec58
thomas-michels <thomasmichels.bnu@gmail.com>
2020-01-24 08:24:48 -0300

    classes controllers 100% terminadas

--- Aula38/View/console.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append('C:/Users/900164/Documents/hbsis/hbsis/Aula34')
-from Controller.squad_controller import SquadController
-
-squad = SquadController()
-squad.adicionar("teste web", "ddd", 33, "java", "angular")
-
