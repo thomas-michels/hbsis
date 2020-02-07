26a8211a9845c212c46d5153b2ca21df3f92be26
thomas-michels <thomasmichels.bnu@gmail.com>
2020-02-07 11:44:22 -0300

    DAO orientado a objeto completo

--- Aula43/DAO/cerveja_dao.py
+++ Aula43/DAO/cerveja_dao.py
@@ -10,24 +10,19 @@ class CervejaDAO(BaseDAO):
     def __init__(self):
         super().__init__()
 
-    def list_all(self):
+    def list_all(self) -> dict:
         return super().list_all(CervejaModel)
 
-    def get_id(self, id):
+    def get_id(self, id) -> dict:
         return super().get_id(CervejaModel, id)
 
-    def insert(self, cerveja:CervejaModel):
-        objeto = super().insert(cerveja)
-        return objeto.as_dict()
+    def insert(self, cerveja:CervejaModel) -> dict:
+        return super().insert(cerveja).as_dict()
 
-    def update(self, cerveja:CervejaModel):
-        query = self.party.query(CervejaModel).filter(CervejaModel.id == cerveja.id)
-        query.update({CervejaModel.marca: cerveja.marca,
-                      CervejaModel.abv: cerveja.abv,
-                      CervejaModel.ibu: cerveja.ibu,
-                      CervejaModel.ebc: cerveja.ebc}, synchronize_session = False)
+    def update(self, cerveja:CervejaModel) -> dict:
+        self.party.merge(cerveja)
         self.party.commit()
         return cerveja.as_dict()
 
-    def remove(self, id):
+    def remove(self, id) -> str:
         return super().remove(CervejaModel, id)
